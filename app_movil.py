582bbb8fb2990bb7ce511a49f9e18903b9dcff79
jormontoya-lgtm <jormontoya@gmail.com>
2026-04-13 21:27:48 -0600

    Add files via upload

--- app_movil.py
+++ app_movil.py
@@ -7,7 +7,12 @@ import base64
 from docx import Document
 
 # --- CONFIGURACIÓN ---
-USUARIOS_PERMITIDOS = {"jorge": "1234", "supervisor": "obra2026", "Gerardo": "123456", "Julie": "101112", "Diego": "202122"}
+USUARIOS_PERMITIDOS = {"jorge": "1234",
+                        "supervisor": "obra2026", 
+                        "gerardo" : "gerardo2026", 
+                        "Julie": "2608",
+                        "Diego": "diego2026"}
+
 STOCK_MINIMO = 20 
 
 st.set_page_config(page_title="SGO-H Pro", layout="centered")
